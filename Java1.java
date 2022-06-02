bd2858cbb8d42c084de699ebb51dbbf3fd36395e
burhan88 <mburhanuddin2@gmail.com>
2022-06-02 11:28:49 +0700

    Update Java1.java

--- Java1.java
+++ Java1.java
@@ -1,11 +1,12 @@
 public class Java1 {
     public static void main(String[] args) {
+        System.out.println("Angka");
         for(int i = 1; i <= 50; i++) {
-            if (((i % 3) == 0) && ((i % 5) == 0))
-                System.out.print("Angka");
+            if (((i % 3) == 0) && ((i % 5) == 0));
             else if ((i % 3) == 0) System.out.println("Habis Dibagi 3");
             else if ((i % 5) == 0) System.out.println("Habis Dibagi 5");
             else System.out.println(i);
         }
+        System.out.println();
     }
 }
