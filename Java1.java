7588a15e9d6d68518c7910046df2a40407ef6ee5
burhan88 <mburhanuddin2@gmail.com>
2022-06-02 11:29:27 +0700

    Update Java1.java

--- Java1.java
+++ Java1.java
@@ -1,6 +1,6 @@
 public class Java1 {
     public static void main(String[] args) {
-        System.out.println("Angka");
+        System.out.println("Bilangan Habis dibagi 3 dan 5");
         for(int i = 1; i <= 50; i++) {
             if (((i % 3) == 0) && ((i % 5) == 0));
             else if ((i % 3) == 0) System.out.println("Habis Dibagi 3");
