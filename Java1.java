353d695fbc11d0239510c7aa4d12951287e93158
burhan88 <mburhanuddin2@gmail.com>
2022-06-02 11:16:49 +0700

    Update Java1.java

--- Java1.java
+++ Java1.java
@@ -2,7 +2,7 @@ public class angka {
   public static void main(String[] args) {
     for(int i = 1; i <= 50; i++) {
       if (((i % 3) == 0) && ((i % 5) == 0))
-        System.out.print("seto");    
+        System.out.print("Angka");    
       else if ((i % 3) == 0) System.out.print("Habis Dibagi 3");
       else if ((i % 5) == 0) System.out.print("Habis Dibagi 5");
       else System.out.print(i);
