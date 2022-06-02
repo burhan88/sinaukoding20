4a7c3768a789490a5cfe77511ebaf059a68993ff
burhan88 <mburhanuddin2@gmail.com>
2022-06-02 11:59:06 +0700

    Update Java1.java

--- Java1.java
+++ Java1.java
@@ -3,8 +3,8 @@ public class Java1 {
         System.out.println("Bilangan Habis dibagi 3 dan 5");
         for(int i = 1; i <= 50; i++) {
             if (((i % 3) == 0) && ((i % 5) == 0));
-            else if ((i % 3) == 0) System.out.println("Habis Dibagi 3");
-            else if ((i % 5) == 0) System.out.println("Habis Dibagi 5");
+            else if ((i % 3) == 0) System.out.println( i +  " Habis Dibagi 3");
+            else if ((i % 5) == 0) System.out.println( i +  " Habis Dibagi 5");
             else System.out.println(i);
         }
         System.out.println();
